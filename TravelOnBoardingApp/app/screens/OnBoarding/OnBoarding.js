c1266b936fc36848a4b6bc29716503401e179a84
sangzun-han <hsjun1996@naver.com>
2021-07-11 16:10:29 +0900

    init

---
+++ TravelOnBoardingApp/app/screens/OnBoarding/OnBoarding.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, StyleSheet, Text} from 'react-native';
+
+const OnBoarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>스크린!!!!!!!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default OnBoarding;
