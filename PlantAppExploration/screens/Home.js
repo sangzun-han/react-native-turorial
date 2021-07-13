14dac5ca53c28104919026f767c87e8b55ce257a
sangzun-han <hsjun1996@naver.com>
2021-07-13 22:22:20 +0900

    UPDATE : dashboard

--- PlantAppExploration/screens/Home.js
+++ PlantAppExploration/screens/Home.js
@@ -34,7 +34,7 @@ const Home = () => {
       id: 3,
       name: '식물4',
       img: images.plant4,
-      favourite: false,
+      favourite: true,
     },
   ]);
 
@@ -55,6 +55,37 @@ const Home = () => {
             borderRadius: 15,
           }}
         />
+        <View
+          style={{
+            position: 'absolute',
+            bottom: '17%',
+            right: 0,
+            backgroundColor: COLORS.primary,
+            paddingHorizontal: SIZES.base,
+            borderTopLeftRadius: 10,
+            borderBottomLeftRadius: 10,
+          }}>
+          <Text style={{color: COLORS.white, ...FONTS.body4}}>{item.name}</Text>
+        </View>
+
+        <View
+          style={{
+            position: 'absolute',
+            top: '15%',
+            left: 7,
+          }}
+          onPress={() => {
+            console.log('Favourite On Pressed');
+          }}>
+          <Image
+            source={item.favourite ? icons.heartRed : icons.heartGreenOutline}
+            resizeMode="contain"
+            style={{
+              width: 20,
+              height: 20,
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -111,7 +142,74 @@ const Home = () => {
       </View>
 
       {/* today's share */}
-      <View style={{height: '50%', backgroundColor: COLORS.lightGray}}></View>
+      <View style={{height: '50%', backgroundColor: COLORS.lightGray}}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomLeftRadius: 50,
+            borderBottomRightRadius: 50,
+            backgroundColor: COLORS.white,
+          }}>
+          <View
+            style={{marginTop: SIZES.font, marginHorizontal: SIZES.padding}}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}>
+              <Text style={{color: COLORS.secondary, ...FONTS.h2}}>
+                오늘의 식물
+              </Text>
+              <TouchableOpacity
+                onPress={() => {
+                  console.log('모두보기 클릭');
+                }}>
+                <Text style={{color: COLORS.secondary, ...FONTS.h3}}>
+                  모두 보기
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              height: '88%',
+              marginTop: SIZES.base,
+            }}>
+            <View style={{flex: 1, backgroundColor: 'red'}}>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                }}
+                onPress={() => {
+                  console.log('식물 클릭');
+                }}>
+                <Image
+                  source={images.plant5}
+                  resizeMode="cover"
+                  style={{width: '100%', height: '100%', borderRadius: 20}}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  marginTop: SIZES,
+                }}
+                onPress={() => {
+                  console.log('식물 클릭');
+                }}>
+                <Image
+                  source={images.plant5}
+                  resizeMode="cover"
+                  style={{width: '100%', height: '100%', borderRadius: 20}}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={{flex: 1.3, backgroundColor: 'yellow'}}></View>
+          </View>
+        </View>
+      </View>
 
       {/* 친구추가 */}
       <View style={{height: '20%', backgroundColor: COLORS.lightGray}}></View>
