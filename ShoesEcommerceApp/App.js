87112c7aec8e02907acbfd4e6fb3195c92175329
sangzun-han <hsjun1996@naver.com>
2021-08-16 18:40:47 +0900

    Home/dashboard UI

--- ShoesEcommerceApp/App.js
+++ ShoesEcommerceApp/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Image, Touchable, TouchableOpacity} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 
 //screens
 import {Home} from './screens';
 
+import {icons, COLORS, FONTS, SIZES} from './constants';
 const theme = {
   ...DefaultTheme,
   colors: {
@@ -19,7 +21,49 @@ const App = () => {
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'SHOE SELECTOR ',
+            headerTintColor: COLORS.lightGray,
+            headerTitleStyle: {
+              ...FONTS.navTitle,
+            },
+            headerLeft: ({onPress}) => (
+              <TouchableOpacity
+                style={{
+                  marginLeft: SIZES.padding,
+                }}
+                onPress={onPress}>
+                <Image
+                  source={icons.menu}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                  }}
+                />
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity
+                style={{
+                  marginRight: SIZES.padding,
+                }}
+                onPress={() => console.log('Header right on pressed')}>
+                <Image
+                  source={icons.search}
+                  resizeMode="contain"
+                  style={{
+                    width: 30,
+                    height: 30,
+                  }}
+                />
+              </TouchableOpacity>
+            ),
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
