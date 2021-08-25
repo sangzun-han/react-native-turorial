5ce3f8def83c29655c24157fb30e49a8fe3c0cb2
sangzun-han <hsjun1996@naver.com>
2021-08-25 10:12:32 +0900

    Update : Finish detail UI

--- CryptoCurrencyApp/screens/CryptoDetail.js
+++ CryptoCurrencyApp/screens/CryptoDetail.js
@@ -20,7 +20,7 @@ import {VictoryCustomTheme} from '../styles';
 
 import {useEffect} from 'react/cjs/react.development';
 
-import {HeaderBar, CurrencyLabel} from '../components';
+import {HeaderBar, CurrencyLabel, TextButton, PriceAlert} from '../components';
 
 import {dummyData, COLORS, SIZES, FONTS, icons} from '../constants';
 
@@ -30,10 +30,70 @@ const CryptoDetail = ({route, navigation}) => {
 
   const [selectedCurrency, setSelectedCurrency] = useState(null);
 
+  const [chartOptions, setChartOptions] = useState(dummyData.chartOptions);
+  const [selectedOption, setSelectedOption] = useState(chartOptions[0]);
+
   useEffect(() => {
     const {currency} = route.params;
     setSelectedCurrency(currency);
   }, []);
+
+  function optionOnClickHandler(option) {
+    setSelectedOption(option);
+  }
+
+  function renderDots() {
+    const dotPosition = Animated.divide(scrollX, SIZES.width);
+
+    return (
+      <View
+        style={{
+          height: 30,
+          marginTop: 15,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          {numberofCharts.map((item, index) => {
+            const opacity = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [0.3, 1, 0.3],
+              extrapolate: 'clamp',
+            });
+
+            const dotSize = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
+              extrapolate: 'clamp',
+            });
+
+            const dotColor = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [COLORS.gray, COLORS.primary, COLORS.gray],
+              extrapolate: 'clamp',
+            });
+
+            return (
+              <Animated.View
+                key={`dot-${index}`}
+                opacity={opacity}
+                style={{
+                  borderRadius: SIZES.radius,
+                  marginHorizontal: 6,
+                  width: dotSize,
+                  height: dotSize,
+                  backgroundColor: dotColor,
+                }}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
 
   function renderChart() {
     return (
@@ -147,7 +207,122 @@ const CryptoDetail = ({route, navigation}) => {
           ))}
         </Animated.ScrollView>
         {/* options */}
+        <View
+          style={{
+            width: '100%',
+            paddingHorizontal: SIZES.padding,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          {chartOptions.map(option => {
+            return (
+              <TextButton
+                key={`option-${option.id}`}
+                label={option.label}
+                customContainerStyle={{
+                  height: 30,
+                  width: 60,
+                  borderRadius: 15,
+                  backgroundColor:
+                    selectedOption.id == option.id
+                      ? COLORS.primary
+                      : COLORS.lightGray,
+                }}
+                customLabelStyle={{
+                  color:
+                    selectedOption.id == option.id ? COLORS.white : COLORS.gray,
+                  ...FONTS.body5,
+                }}
+                onPress={() => optionOnClickHandler(option)}
+              />
+            );
+          })}
+        </View>
         {/* dots */}
+        {renderDots()}
+      </View>
+    );
+  }
+
+  function renderBuy() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding,
+          marginHorizontal: SIZES.radius,
+          padding: SIZES.radius,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.white,
+          ...styles.shadow,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: SIZES.radius,
+          }}>
+          {/* Currency */}
+          <View
+            style={{
+              flex: 1,
+            }}>
+            <CurrencyLabel
+              icon={selectedCurrency?.image}
+              currency={`${selectedCurrency?.currency} Wallet`}
+              code={selectedCurrency?.code}
+            />
+          </View>
+          {/* Amount */}
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <View style={{marginRight: SIZES.base}}>
+              <Text style={{...FONTS.h3}}>
+                ${selectedCurrency?.wallet.value}
+              </Text>
+              <Text
+                style={{
+                  textAlign: 'right',
+                  color: COLORS.gray,
+                  ...FONTS.body4,
+                }}>
+                {selectedCurrency?.wallet.crypto} {selectedCurrency?.code}
+              </Text>
+            </View>
+            <Image
+              source={icons.right_arrow}
+              resizeMode="cover"
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: COLORS.gray,
+              }}
+            />
+          </View>
+        </View>
+        <TextButton
+          label="Buy"
+          onPress={() =>
+            navigation.navigate('Transaction', {currency: selectedCurrency})
+          }
+        />
+      </View>
+    );
+  }
+
+  function renderAbout() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding,
+          marginHorizontal: SIZES.radius,
+          padding: SIZES.radius,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.white,
+          ...styles.shadow,
+        }}>
+        <Text style={{...FONTS.h3}}>About {selectedCurrency?.currency}</Text>
+        <Text style={{marginTop: SIZES.base, ...FONTS.body3}}>
+          {selectedCurrency?.description}
+        </Text>
       </View>
     );
   }
@@ -166,6 +341,14 @@ const CryptoDetail = ({route, navigation}) => {
             paddingBottom: SIZES.padding,
           }}>
           {renderChart()}
+          {renderBuy()}
+          {renderAbout()}
+          <PriceAlert
+            customContainerStyle={{
+              marginTop: SIZES.padding,
+              marginHorizontal: SIZES.radius,
+            }}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
